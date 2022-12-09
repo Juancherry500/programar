dd3249794166387acd3985f33d495daed95ded43
Juan Sanchez <juanra500@hotmail.com>
2022-12-09 19:07:53 -0300

    Escuchando eventos con JavaScript, Clase 19

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,6 +1,21 @@
-function seleccionarMascotaJugador(){
-    alert("Selecionaste a tu mascota");
+function iniciarJuego(){
+    let botonMascotaJugador = document.getElementById("boton-mascota");
+    botonMascotaJugador.addEventListener("click",seleccionarMascotaJugador);
+
 }
 
-let botonMascotaJugador = document.getElementById("boton-mascota");
-botonMascotaJugador.addEventListener("click",seleccionarMascotaJugador);
+function seleccionarMascotaJugador(){
+    
+    let hipo = document.getElementById("hipodoge").checked
+    let capi = document.getElementById("capipepo").checked
+    let rati = document.getElementById("ratigueya").checked
+    if (hipo){
+        alert("Selecionaste a Hipodoge")
+    }else if (capi){
+        alert("Selecionaste a Capipepo")
+    }else if (rati){
+        alert("Selecionaste a   Ratigueya")
+    }
+}
+
+window.addEventListener("load",iniciarJuego)
