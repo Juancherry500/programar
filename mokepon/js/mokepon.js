c69ac33d14ccef6a1e5a7c61828b4ff6fbe279a6
Juan Sanchez <juanra500@hotmail.com>
2022-12-11 01:11:26 -0300

    Ataques en JavaScript, Clase 23

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,9 +1,28 @@
+let ataqueJuagador;
+
 function iniciarJuego(){
     let botonMascotaJugador = document.getElementById("boton-mascota");
     botonMascotaJugador.addEventListener("click",seleccionarMascotaJugador);
 
+    let botonAtaqueFuego = document.getElementById("boton-fuego");
+    botonAtaqueFuego.addEventListener("click",ataqueFuego);
+    let botonAtaqueAgua = document.getElementById("boton-agua");
+    botonAtaqueAgua.addEventListener("click",ataqueAgua);
+    let botonAtaqueTierra = document.getElementById("boton-tierra");
+    botonAtaqueTierra.addEventListener("click",ataqueTierra);
 }
-
+function ataqueFuego(){
+    ataqueJuagador="FUEGO";
+    alert(ataqueJuagador);
+}
+function ataqueAgua(){
+    ataqueJuagador="AGUA";
+    alert(ataqueJuagador);
+}
+function ataqueTierra(){
+    ataqueJuagador="TIERRA";
+    alert(ataqueJuagador);
+}
 function seleccionarMascotaJugador(){
     
     let inputHipodoge = document.getElementById("hipodoge");
@@ -26,12 +45,12 @@ function seleccionarMascotaJugador(){
 
 function seleccionarMascotaEnemigo(){
 
-    let ataqueAleatorio=aleatorio(1,3);
+    let seleccionAleatorio=aleatorio(1,3);
     let spanMascotaEnemigo=document.getElementById("masota-enemigo");
 
-    if (ataqueAleatorio == 1){
+    if (seleccionAleatorio == 1){
         spanMascotaEnemigo.innerHTML="Hipodoge";
-    }else if (ataqueAleatorio == 2){
+    }else if (seleccionAleatorio == 2){
         spanMascotaEnemigo.innerHTML="Capipepo";
     }else {
         spanMascotaEnemigo.innerHTML="Ratigueya";
